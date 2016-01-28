98e5fc5c9f0a57549eca5b11c3313843d017f188
Hanzhou Shi <hanzhou87@gmail.com>
2016-01-27 23:35:51 -0800

    Added .emacs to setup, restarted.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-ln -s ~/Configurations/dotfile/.bashrc ~/.bashrc 
-ln -s ~/Configurations/dotfile/.vimrc ~/.vimrc
+ln -s ~/Configurations/dotfile/bashrc ~/.bashrc 
+ln -s ~/Configurations/dotfile/vimrc ~/.vimrc
+ln -s ~/Configurations/dotfile/emacs ~/.emacs
 ln -s ~/Configurations/vim ~/.vim
