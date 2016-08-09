de8c11c39a50f5a1cd45f052087fac4a7737487e
hanjoes <hanzhou87@gmail.com>
2016-08-09 15:53:09 -0700

    Tweak configurations.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ ln -s ~/Configurations/dotfile/bashrc ~/.bashrc
 ln -s ~/Configurations/dotfile/vimrc ~/.vimrc
 ln -s ~/Configurations/dotfile/emacs ~/.emacs
 ln -s ~/Configurations/vim ~/.vim
+
+# Setting up git aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
