587b70834bf242fbd82462eeff46eaa3d50c9ead
hanjoes <hanzhou87@gmail.com>
2017-01-06 23:50:14 -0800

    small tweaks

--- dotfile/emacs.el
+++ dotfile/emacs.el
@@ -1,22 +1,21 @@
 ;; PATH
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 
+;; custom key bindings
+(global-set-key (kbd "C-c C-n C-t") 'neotree-toggle) ; toggle neotree
+
 ;; package manager
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
-;; temporary files
-;; (setq backup-directory-alist
-;;   '((".*" . ,"/Users/hanzhos/Documents/emacs-tmp")))
-;; (setq auto-save-file-name-transforms
-;;   '((".*" ,"/Users/hanzhos/Documents/emacs-tmp" t)))
-
 ;; initialization stuff
 (global-linum-mode 1)
 (add-hook 'after-init-hook 'global-company-mode)
-(add-hook 'after-init-hook 'neotree)
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+;; font size
+(set-face-attribute 'default (selected-frame) :height 140)
 
 ;; yaml mode
 (require 'yaml-mode)
@@ -25,7 +24,6 @@
 ;; blank mode
 (require 'blank-mode)
 (global-blank-mode 1)
-
 
 ;; highlight the current line
 (require 'hlinum)
@@ -38,10 +36,6 @@
 
 ;; show paren mode (display parentheses)
 (show-paren-mode t)
-
-;; blank mode
-(require 'blank-mode)
-(global-blank-mode 1)
 
 ;; swift mode
 (add-hook 'swift-mode-hook (lambda() (setq indent-tabs-mode t)))
@@ -67,6 +61,14 @@
 (add-to-list 'load-path "/usr/local/lib/node_modules/tern/emacs/")
 (add-hook 'js-mode-hook (lambda () (tern-mode t)))
 
+;; NeoTree
+(add-hook 'neotree-mode-hook
+          (lambda ()
+            (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+            (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+            (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+            (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+
 ;; enable js2-mode by default (or it will be js-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
@@ -80,40 +82,6 @@
  '(blank-chars
    (quote
     (tabs spaces trailing space-before-tab indentation empty space-after-tab)))
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes nil)
- '(custom-safe-themes
-   (quote
-    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(fci-rule-color "#073642")
- '(haskell-indentation-layout-offset 2)
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors
-   (quote
-    (("#073642" . 0)
-     ("#546E00" . 20)
-     ("#00736F" . 30)
-     ("#00629D" . 50)
-     ("#7B6000" . 60)
-     ("#8B2C02" . 70)
-     ("#93115C" . 85)
-     ("#073642" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
- '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
  '(indent-tabs-mode nil)
  '(magit-diff-use-overlays nil)
  '(markdown-command "/usr/local/bin/markdown")
@@ -122,53 +90,4 @@
  '(neo-theme (quote classic))
  '(neo-window-fixed-size t)
  '(neo-window-width 35)
- '(nrepl-message-colors
-   (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
- '(pos-tip-background-color "#073642")
- '(pos-tip-foreground-color "#93a1a1")
- '(shell-file-name "/bin/bash")
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
- '(term-default-bg-color "#002b36")
- '(term-default-fg-color "#839496")
- '(vc-annotate-background nil)
- '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#c85d17")
-     (60 . "#be730b")
-     (80 . "#b58900")
-     (100 . "#a58e00")
-     (120 . "#9d9100")
-     (140 . "#959300")
-     (160 . "#8d9600")
-     (180 . "#859900")
-     (200 . "#669b32")
-     (220 . "#579d4c")
-     (240 . "#489e65")
-     (260 . "#399f7e")
-     (280 . "#2aa198")
-     (300 . "#2898af")
-     (320 . "#2793ba")
-     (340 . "#268fc6")
-     (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
- '(xterm-color-names
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
- '(xterm-color-names-bright
-   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Menlo"))))
- '(blank-line ((t (:background "gray20" :foreground "violet")))))
-
-(load-theme 'solarized-light)
-
-(this is an unquoted list)
+ '(shell-file-name "/bin/bash"))
